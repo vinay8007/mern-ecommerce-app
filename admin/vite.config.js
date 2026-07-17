b1cb477df4e1ee3c9d01f57ea92ec84b59dcfe31
8007vinay <vksingh82100@gmail.com>
2026-07-17 23:05:39 +0530

    Initial commit: Full Stack Ecommerce Application

---
+++ admin/vite.config.js
@@ -0,0 +1,28 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {port: 5174}
+
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  server: {
+    port: 5174,
+  },
+})
